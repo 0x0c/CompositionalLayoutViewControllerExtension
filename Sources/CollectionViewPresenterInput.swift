c7270717ca354a9f6b8ed4c30a6d0c3dbb994aae
Akira <akira.matsuda@me.com>
2023-06-08 01:09:15 +0900

    Remove unused method

--- Sources/CollectionViewPresenterInput.swift
+++ Sources/CollectionViewPresenterInput.swift
@@ -12,6 +12,5 @@ import UIKit
 public protocol CollectionViewPresenterInput: AnyObject {
     var sections: [CollectionViewSection] { get }
 
-    func section(for sectionIndex: Int) -> CollectionViewSection
     func didItemSelect(indexPath: IndexPath)
 }
