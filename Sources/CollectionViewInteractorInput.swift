b0b0e6a55f9336f662da3c06be090f84ea5ab4e1
Akira <akira.matsuda@me.com>
2023-07-18 23:33:45 +0900

    Remove unnecessarily methods

--- Sources/CollectionViewInteractorInput.swift
+++ Sources/CollectionViewInteractorInput.swift
@@ -10,22 +10,4 @@ import UIKit
 
 public protocol CollectionViewInteractorInput: AnyObject {
     var sections: [CollectionViewSection]? { get set }
-
-    func store(_ sections: [CollectionViewSection]?)
-    func store(@CollectionViewSectionBuilder _ sections: () -> [CollectionViewSection])
-    func section(for sectionIndex: Int) -> CollectionViewSection?
-}
-
-public extension CollectionViewInteractorInput {
-    func store(_ sections: [CollectionViewSection]?) {
-        self.sections = sections
-    }
-
-    func store(@CollectionViewSectionBuilder _ sections: () -> [CollectionViewSection]) {
-        self.sections = sections()
-    }
-
-    func section(for sectionIndex: Int) -> CollectionViewSection? {
-        return sections?[sectionIndex]
-    }
 }
